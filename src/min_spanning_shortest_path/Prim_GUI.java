f42fb5d9ec3037008c2bc59504a2d2b70b4c6523
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2020-05-04 16:15:18 -0500

    Create Prim_GUI.java

---
+++ src/min_spanning_shortest_path/Prim_GUI.java
@@ -0,0 +1,43 @@
+package min_spanning_shortest_path;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Prim_GUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Prim_GUI window = new Prim_GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Prim_GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
