114542fd30dab417bdb833d10e5daf32d4f3933a
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2020-05-04 14:58:06 -0500

    Create Prim_Min_Span_Tree.java

---
+++ src/min_spanning_shortest_path/Prim_Min_Span_Tree.java
@@ -0,0 +1,14 @@
+package min_spanning_shortest_path;
+
+public class Prim_Min_Span_Tree {
+
+	public Prim_Min_Span_Tree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) { // For testing purposes
+		// TODO Auto-generated method stub
+
+	}
+
+}
